b77911cdbe14b66c755c7eb3846a79161991b611
Mohamed-elhediKraiem <113349583+Mohamed-elhediKraiem@users.noreply.github.com>
2025-08-04 16:04:35 +0200

    Add interactive map of sales by postal code using pgeocode and Streamlit

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import os
+import pgeocode
 
 st.title("📈 Évolution des commandes par année")
 file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'Sample - Superstore.csv')
@@ -35,3 +36,30 @@ fig2 = px.line(x=ventes_par_annee.index,
                title=f"Évolution des ventes - {pays}")
 
 st.plotly_chart(fig2)
+
+###############################################
+#####################################MAP
+
+postal_codes = df["Postal Code"].astype(str).tolist()
+nomi = pgeocode.Nominatim('us')  # 'us' = United States
+us_post = nomi.query_postal_code(postal_codes)
+
+# Ajouter latitude et longitude dans ton DataFrame
+df['latitude'] = us_post['latitude'].values
+df['longitude'] = us_post['longitude'].values
+
+df_geo = df.groupby(['City','latitude', 'longitude']).agg({
+    'Sales': 'sum',
+    'Postal Code': 'count'  # nombre de commandes par zone
+}).rename(columns={'Postal Code': 'Nb_commandes'}).reset_index()
+
+fig = px.scatter_mapbox(df_geo,
+                        lat='latitude', lon='longitude',
+                        size='Sales',
+                        color='Sales',
+                        hover_name='City',
+                        mapbox_style="open-street-map",
+                        zoom=3,
+                        title="Carte des ventes par code postal")
+
+st.plotly_chart(fig)
